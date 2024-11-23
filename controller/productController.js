4d222e583bbe41cb378cb96bf0ecc5be43781d77
heladixman <hdixman@gmail.com>
2024-11-23 16:56:26 +0700

    adding several file

--- controller/productController.js
+++ controller/productController.js
@@ -1,5 +1,4 @@
 const db = require('../config/firebase')
-const User = require('../model/User')
 
 class UserController{
 
--- model/User.js
+++ model/User.js
@@ -1,16 +1,19 @@
 class User {
-    constructor(id,uuid, name, email, confirm_email, phone, confirm_phone, age, gender, skin_type, role, profile){
-        this.id = id
-        this.uuid = uuid
-        this.name = name
-        this.email = email
-        this.confirm_email = confirm_email
+    constructor(name, email, confirm_email, phone, confirm_phone, age, gender, skin_type, role, profile_image, settings, hasStore){
+        const bucketName = process.env.BUCKET_NAME || 'foskin-storage'
+        this.name = name || null
+        this.email = email | null
+        this.confirm_email = confirm_email || 0
         this.phone = phone
-        this.confirm_phone = confirm_phone
-        this.age = age
-        this.gender = gender
-        this.skin_type = skin_type
+        this.confirm_phone = confirm_phone || 1
+        this.age = age || 0
+        this.gender = gender || 'male'
+        this.skin_type = skin_type || 'normal'
         this.role = role || 'user'
-        this.profile = profile
+        this.profile_image = profile_image || `storage.googleapis.com/${bucketName}/images/default_profile.jpg`
+        this.settings = settings
+        this.hasStore = hasStore
+        this.create_at = this.create_at ||new Date()
+        this.update_at = new Date()
     }
 }
